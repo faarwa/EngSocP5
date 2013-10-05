2448b44cb7c7ec2644bdc402ac4307daa3837ffd
nfarwa <farwa.naqi@gmail.com>
2013-10-04 23:26:58 -0400

    Added class points in Points tab.

---
+++ EngSocP5/ESParticipationViewController.h
@@ -0,0 +1,13 @@
+//
+//  ESParticipationViewController.h
+//  EngSocP5
+//
+//  Created by Farwa Naqi on 2013-10-04.
+//  Copyright (c) 2013 Farwa Naqi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ESParticipationViewController : UIViewController
+
+@end
---
+++ EngSocP5/ESParticipationViewController.m
@@ -0,0 +1,38 @@
+//
+//  ESParticipationViewController.m
+//  EngSocP5
+//
+//  Created by Farwa Naqi on 2013-10-04.
+//  Copyright (c) 2013 Farwa Naqi. All rights reserved.
+//
+
+#import "ESParticipationViewController.h"
+
+@interface ESParticipationViewController ()
+
+@end
+
+@implementation ESParticipationViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
